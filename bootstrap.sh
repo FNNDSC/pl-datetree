eac06cc100e102d9517068d367d11423f5ef3a47
Vaikunthan Mathiyalakan <vaimathiyalakan@gmail.com>
2024-08-09 14:57:21 -0400

    Configure python-chrisapp-template/bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,9 +15,9 @@
 # ----------------------------------------
 
 PLUGIN_NAME="$(basename $(dirname $(realpath $0)))"  # name of current directory
-PLUGIN_TITLE='My ChRIS Plugin'
-SCRIPT_NAME='commandname'
-DESCRIPTION='A ChRIS plugin to do something awesome'
+PLUGIN_TITLE='Datetree'
+SCRIPT_NAME='datetree'
+DESCRIPTION='A file directory script that takes in a date range and output directory and creates a nested dir structure'
 ORGANIZATION='FNNDSC'
 EMAIL='dev@babyMRI.org'
 
@@ -46,7 +46,7 @@ ENABLE_ACTIONS_BUILD=yes
 # STEP 2. Uncomment the line where it says READY=yes
 # ----------------------------------------
 
-#READY=yes
+READY=yes
 
 # ----------------------------------------
 # STEP 3. Run: ./bootstrap.sh
